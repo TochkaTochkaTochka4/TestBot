6d3af3c879d6b49a1c1376af5cc876c0336c8e0f
TochkaTochkaTochka4 <shinkarenkovova468@gmail.com>
2026-02-06 02:56:11 +0200

    Create bot.py
    Добавлен файл bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Вставь сюда токен от BotFather
+TOKEN = 7960755516:AAHWipFJsTeMOzE3ZCs1aDVjNhbtzWl9Hvk
+
+# Команда /start
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Привет! Я твой тестовый бот 😎")
+
+# Повторяем любое сообщение пользователя
+def echo(update: Update, context: CallbackContext):
+    update.message.reply_text(f"Ты написал: {update.message.text}")
+
+# Настройка бота
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+# Запуск бота
+updater.start_polling()
+updater.idle()
